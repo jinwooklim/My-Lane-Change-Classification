d8c42947e21d6b48a28c222b1d3923dc2dd5c6be
jinwooklim <aiden.limo@outlook.com>
2020-02-14 11:50:40 +0900

    Add data_preprocess

---
+++ data_preprocess.py
@@ -0,0 +1,75 @@
+import os
+import numpy as np
+import cv2
+import parmap
+from multiprocessing import Manager
+
+data_path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/'
+NIGHT_THRES = 40.0
+
+def remove_none_dir(chunk, sub_chunk_list):
+    for sub_chunk in sub_chunk_list:
+        if not os.path.isdir(os.path.join(data_path, chunk, sub_chunk)):
+            print("No Dir:", sub_chunk)
+            sub_chunk_list.remove(sub_chunk)
+        else:
+            pass
+    return sub_chunk_list
+
+
+def extract_frame(input_path, image_path, only_day_scene=True):
+    path, file_name = os.path.split(input_path)
+
+    preview_path = os.path.join(path, 'preview.png')
+    preview_img = cv2.imread(preview_path)
+    if np.mean(preview_img) < NIGHT_THRES:
+        return
+
+    path = path.split(data_path)
+    path = path[1]
+    extract_path = os.path.join(image_path, path)
+    if not os.path.exists(extract_path):
+        os.makedirs(extract_path)
+
+    p = path.split('/')
+    print("Read Video : %s || %s || %s"%(p[0], p[1], p[2]))
+    hevc_video = cv2.VideoCapture(input_path)
+    id = 0
+    while(hevc_video.isOpened()):
+        ret, frame = hevc_video.read()
+        if ret == True:
+            cv2.imwrite(os.path.join(extract_path, '%06d.png'%(id)), frame)
+            id += 1
+        else:
+            break
+    hevc_video.release()
+
+
+if __name__ == "__main__":
+    dir_name = os.path.join(os.getcwd(),'comma2k19_test')
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    image_path = os.path.join(dir_name, 'input')
+    masked_image_path = os.path.join(dir_name, 'masked_images')
+    if not os.path.exists(image_path):
+        os.mkdir(image_path)
+    if not os.path.exists(masked_image_path):
+        os.mkdir(masked_image_path)
+
+    total_video_list = []
+    chunk_list = os.listdir(data_path)
+    for chunk in chunk_list:
+        sub_chunk_list = os.listdir(os.path.join(data_path, chunk))
+        sub_chunk_list = remove_none_dir(chunk, sub_chunk_list)
+        for sub_chunk in sub_chunk_list:
+            video_list = os.listdir(os.path.join(data_path, chunk, sub_chunk))
+            # print(video_list)
+            for video in video_list:
+                video_path = os.path.join(data_path, chunk, sub_chunk, video, 'video.hevc')
+                total_video_list.append(video_path)
+    total_video_list.sort()
+    print(len(total_video_list))
+
+    ## Multi-processing
+    num_cores = 10
+    parmap.map(extract_frame, total_video_list, image_path, True, pm_pbar=True, pm_processes=num_cores)
--- test_mask_rcnn.py
+++ test_mask_rcnn.py
@@ -14,7 +14,25 @@ if not os.path.exists(image_path):
 if not os.path.exists(masked_image_path):
     os.mkdir(masked_image_path)
 
-path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-08-17--14-55-39/1'
+## Day Scene
+# path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-08-17--14-55-39/1'
+# path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-08-17--14-17-47/5'
+
+## Night Scene
+# path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-07-27--06-03-57/6' # 35.86652295800011
+# path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-07-27--06-50-48/8' # 17
+path ='/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_2/b0c9d2329ad1606b|2018-09-23--23-24-40/10'
+
+## Foggy Scene
+# path = '/home/titan/hdd_ext/hdd2/comma2k19_ext/Chunk_1/b0c9d2329ad1606b|2018-07-29--11-17-20/4' # 80
+
+preview_img = cv2.imread(os.path.join(path, 'preview.png'))
+print(np.mean(preview_img)) # 35.86652295800011
+cv2.imshow('preview.png', preview_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+exit()
+
 hevc_video = cv2.VideoCapture(os.path.join(path, 'video.hevc'))
 id = 0
 print("Read Video ...")
